ba6d41359f8e228fde335bca73dfce8b3ba87f7b
Ari <thearxari@gmail.com>
2024-08-02 15:27:29 +0200

    Update rancat.sh
    10 lines :3

--- rancat.sh
+++ rancat.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-FOLDER_PATH="/path/to/your/foldert" 
-
-if [ ! -d "$FOLDER_PATH" ]; then
-  echo "The specified folder does not exist."
-  exit 1
-fi
+FOLDER_PATH="/path/to/your/folder"
 
 txt_files=("$FOLDER_PATH"/*.txt)
 
-if [ ${#txt_files[@]} -eq 0 ]; then
-  echo "No .txt files found in the specified folder."
-  exit 1
+if [ ${#txt_files[@]} -gt 0 ]; then
+  selected_file="${txt_files[RANDOM % ${#txt_files[@]}]}"
+  cat "$selected_file"
 fi
-
-selected_file="${txt_files[RANDOM % ${#txt_files[@]}]}"
-
-echo "Running 'cat' on: $selected_file"
-cat "$selected_file"
